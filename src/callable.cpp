f22ec37be4fd8d1059995de254ecb283a6637ae6
Nathan Ernst <nathan.ernst@gmail.com>
2022-05-11 19:20:52 -0700

    add dir builtin

--- src/callable.cpp
+++ src/callable.cpp
@@ -1,6 +1,11 @@
+#include <iterator>
+#include <ranges>
+#include <sstream>
 #include "lox/callable.hpp"
 #include "lox/object.hpp"
 #include "lox/builtins/clock.hpp"
+#include "lox/builtins/dir.hpp"
+#include "lox/interpreter.hpp"
 
 
 namespace lox
@@ -8,10 +13,11 @@ namespace lox
 	object callable::operator()(interpreter& inter, std::vector<object> const& arguments) const
 	{ return impl_->call(inter, arguments); }
 
-	std::vector<callable> builtins()
+	std::vector<callable> callable::builtins()
 	{
 		return std::vector<callable>{{
-			{ callable::make<builtin::clock>() },
+			{ callable::make<::lox::builtin::clock>() },
+			{ callable::make<::lox::builtin::dir>() },
 		}};
 	}
 
@@ -26,6 +32,49 @@ namespace lox
 			
 			using seconds = std::chrono::duration<double>;
 			return object{std::chrono::duration_cast<seconds>(tp).count()};
+		}
+
+		object dir::call(interpreter& inter, std::vector<object> const& args)
+		{
+			ignore_unused(args);
+
+			std::vector<std::string> names;
+
+			std::function<void(environment const&)> collector = [&](environment const& e)
+			{
+				auto local{e.names()};
+				std::ranges::sort(local);
+
+				if (names.empty())
+				{
+					names = local;
+				}
+				else
+				{
+					std::vector<std::string> work;
+					std::ranges::merge(names, local, std::back_inserter(work));
+					names = std::move(work);
+				}
+
+				if (e.enclosing())
+					collector(*e.enclosing());
+			};
+
+			collector(inter.current_env());
+
+			std::stringstream out;
+			out << "{";
+			bool need_sep{false};
+			for (auto&& s : names)
+			{
+				if (need_sep)
+					out << ", ";
+				else
+					need_sep = true;
+				out << s;
+			}
+			out << "}";
+			return object{out.str()};
 		}
 	}
 }
